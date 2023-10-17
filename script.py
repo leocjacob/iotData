44a3c3d8b419ae08019a02aa96a1480dfc46b24c
leocjacob <leocjacob@gmail.com>
2023-10-17 03:42:43 -0400

    updated influx dbscript

--- script.py
+++ script.py
@@ -1,17 +1,66 @@
+from influxdb_client import InfluxDBClient
 import json
 from datetime import datetime
+import os
 
-# Generate JSON data with a UTC timestamp
-utc_now = datetime.utcnow()
-data = {
-    "Timestamp": utc_now.strftime("%d%b%Y-%H-%M"),  # Format as "16OCT2023-22-30"
-    "Humidity": 79.80000305,
-    "Temperature": 9.5,
-    "HeatIndex": 8.58913517,
-    "SoilMoisture": 1515
+# Get the current date and time in UTC
+created_on = datetime.utcnow().isoformat()
+
+# InfluxDB 2 connection settings
+url = os.environ.get('INFLUXDB_URL')
+token = os.environ.get('INFLUXDB_TOKEN')
+org = os.environ.get('INFLUXDB_ORG')
+bucket = os.environ.get('INFLUXDB_BUCKET')
+
+# Create an InfluxDB 2 client
+client = InfluxDBClient(url=url, token=token, org=org)
+
+query_api = client.query_api()
+
+query = """from(bucket: "{bucket}")
+  |> range(start: -55h)
+  |> filter(fn: (r) => r["_measurement"] == "HeatIndex" or r["_measurement"] == "Humidity" or r["_measurement"] == "SoilMoisture" or r["_measurement"] == "Temperature")
+  |> map(fn: (r) => ({ r with _time: r._time }))
+  """
+
+tables = query_api.query(query, org="leocj.ca")
+
+
+# Initialize a dictionary to store the data
+output_data = {
+    "MetaData": {
+        "createdOn": created_on
+    },
+    "Temperature": [],
+    "Humidity": [],
+    "HeatIndex": [],
+    "SoilMoisture": []
 }
 
-# Save data to a JSON file
-with open("sensorData.json", "w") as json_file:
-    json.dump(data, json_file)
+# Iterate through the records and organize the data
+for table in tables:
+    for record in table.records:
+        measurement = record.get_measurement()
+        time = record.get_time().isoformat()
+        value = record.get_value()
+        data_point = {"dateTime": time , "value": value}
+        
+        if measurement == "Temperature":
+            output_data["Temperature"].append(data_point)
+        elif measurement == "Humidity":
+            output_data["Humidity"].append(data_point)
+        elif measurement == "HeatIndex":
+            output_data["HeatIndex"].append(data_point)
+        elif measurement == "SoilMoisture":
+            output_data["SoilMoisture"].append(data_point)
 
+# Convert the dictionary to a JSON string
+json_output = json.dumps(output_data, indent=4)
+
+# Save data to a JSON file
+if(tables!=[]):
+  with open("sensorData.json", "w") as json_file:
+      json_file.write(json_output)  # Use write() instead of dump()
+  print("Sensor Data Created.")
+else:
+    print("No Sensor Data")
