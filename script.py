a269e54531e44f8654a39e88ecfaccfcfd4a337e
leocjacob <leocjacob@gmail.com>
2023-10-16 22:37:26 -0400

    updated

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 import json
 
 # Generate JSON data
-data = {"Humidity":79.80000305,"Temperature":9.5,"HeatIndex":8.58913517,"SoilMoisture":1515}
+data = {"Timestamp": datetime.now().strftime("%d%b%Y-%H-%M"),"Humidity":79.80000305,"Temperature":9.5,"HeatIndex":8.58913517,"SoilMoisture":1515}
 
 # Save data to a JSON file
 with open("sensorData.json", "w") as json_file:
