4d02b0930ec5d1a1386a78dcc50465e9a3abb691
leocjacob <leocjacob@gmail.com>
2023-10-16 22:39:24 -0400

    bug fix

--- script.py
+++ script.py
@@ -1,8 +1,16 @@
 import json
+from datetime import datetime
 
-# Generate JSON data
-data = {"Timestamp": datetime.now().strftime("%d%b%Y-%H-%M"),"Humidity":79.80000305,"Temperature":9.5,"HeatIndex":8.58913517,"SoilMoisture":1515}
+# Generate JSON data with a timestamp
+data = {
+    "Timestamp": datetime.now().strftime("%d%b%Y-%H-%M"),  # Format as "16OCT2023-22-30"
+    "Humidity": 79.80000305,
+    "Temperature": 9.5,
+    "HeatIndex": 8.58913517,
+    "SoilMoisture": 1515
+}
 
 # Save data to a JSON file
 with open("sensorData.json", "w") as json_file:
     json.dump(data, json_file)
+
